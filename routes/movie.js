329c034d4c7c33ddc1360bb916afd3041297ec26
baesparza <bruno.be81@gmail.com>
2017-08-15 23:36:03 -0500

    feat(get): get movie by id

--- routes/movie.js
+++ routes/movie.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var db = {};
 
 router.post('/', (req, res, next) => {
-    // console.log('[/movies] POST:', req.body)
+    // console.log('[/movie] POST:', req.body)
     if (!req.body) {
         res.status(403)
             .json({ error: true, message: 'Empty post' });
@@ -20,9 +20,19 @@ router.post('/', (req, res, next) => {
 });
 
 router.get('/', (req, res, next) => {
-    // console.log('[/movies] GET:', req.body)
+    // console.log('[/movie] GET')
     res.status(200)
         .json({ movies: _.values(db) })
 })
+
+router.get('/:id', (req, res, next) => {
+    // console.log(`[/movie/${req.params.id}] GET`)
+    if (!req.params.id) {
+        res.status(403)
+            .json({ error: true, message: 'bad request, empty params:id' })
+    }
+    res.status(200)
+        .json({ movie: db[req.params.id] });
+});
 
 module.exports = router;
